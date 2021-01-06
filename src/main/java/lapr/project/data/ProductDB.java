4f92312a3106543afb3087ac5b9f02b8bd826326
1181180 <1181180@isep.ipp.pt>
2021-01-06 22:44:49 +0000

    MEN09-131 #close Add Product [Implementation]

--- src/main/java/lapr/project/data/ProductDB.java
+++ src/main/java/lapr/project/data/ProductDB.java
@@ -5,6 +5,8 @@
  */
 package lapr.project.data;
 
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
 import lapr.project.model.Product;
 
@@ -14,13 +16,23 @@ import lapr.project.model.Product;
  */
 public class ProductDB {
 
-
+    public Product newProduct(int id, String name, double weight, double price){
+        return new Product(id,  name, weight, price);
+    }
 
     public Product getProduct(int id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public List<String> getProducts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public boolean save(Product product)throws SQLException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int save(HashSet<Product> products) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
