66fab6943702962756d15096abebbaf2d467d261
diogo0620 <1181178@isep.ipp.pt>
2021-01-11 23:33:48 +0000

    MEN09-236 #close Maintenace [Fix bugs]

--- src/main/java/lapr/project/model/Client.java
+++ src/main/java/lapr/project/model/Client.java
@@ -8,7 +8,6 @@ package lapr.project.model;
 import lapr.project.utils.Constants;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -22,7 +21,7 @@ public class Client extends User {
     private final ShoppingCart cart;
     private CreditCard card;
     private Address address;
-    private final List<Order> orders;
+
 
 
 
@@ -46,7 +45,6 @@ public class Client extends User {
         setCard(creditCard);
         setAddress(address);
         cart = new ShoppingCart();
-        orders = new ArrayList<>();
     }
 
 
@@ -117,15 +115,7 @@ public class Client extends User {
         this.points = points;
     }
 
-   /* public Order makeOrder(int id, double totalPrice, HashMap<Product, Integer> items) {
-        Order o = new Order(id, totalPrice, items);
-        orders.add(o);
-        return o;
-    }
 
-    public Phamarcy getNeareastPhamarcy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }*/
 
 
 
