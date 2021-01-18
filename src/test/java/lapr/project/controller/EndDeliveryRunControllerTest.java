364e3f5b847452ea5bf2246b7d889d139877d4bd
1180567(João Pedro Silva) <1180567@isep.ipp.pt>
2021-01-18 18:03:24 +0000

    MEN09-242 #close Maintence [Fix Tests]

---
+++ src/test/java/lapr/project/controller/EndDeliveryRunControllerTest.java
@@ -0,0 +1,57 @@
+package lapr.project.controller;
+
+import lapr.project.data.DeliveryRunDB;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class EndDeliveryRunControllerTest {
+
+    private DeliveryRunDB drdb;
+    private EndDeliveryRunController controller;
+    private static String courier;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        this.drdb = mock(DeliveryRunDB.class);
+        this.courier = "courier1@lapr3.com";
+        this.controller= new EndDeliveryRunController();
+
+
+    }
+    @Test
+    void getScooterCourierByDeliveryRun() throws SQLException {
+        when(drdb.getCourierEmail(anyInt())).thenReturn(courier);
+        String result=drdb.getCourierEmail(1);
+        assertEquals(courier, result);
+    }
+    @Test
+    void getScooterCourierByDeliveryRunFail() throws SQLException {
+        when(drdb.getCourierEmail(anyInt())).thenReturn(null);
+        String result=drdb.getCourierEmail(1);
+        String exp= null;
+        assertEquals(exp, result);
+    }
+
+    @Test
+    void setEndDate() throws SQLException {
+        when(drdb.endDeliveryRun(anyInt())).thenReturn(true);
+        boolean result= drdb.endDeliveryRun(1);
+        boolean exp= true;
+        assertEquals(exp,result);
+    }
+
+    @Test
+    void setEndDateFail() throws SQLException {
+        when(drdb.endDeliveryRun(anyInt())).thenReturn(false);
+        boolean result= drdb.endDeliveryRun(1);
+        boolean exp= false;
+        assertEquals(exp,result);
+    }
+}
